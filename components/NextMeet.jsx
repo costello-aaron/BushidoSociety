6f48f1683688d29ea1c3c35520beca1820967c69
costello-aaron <costello.aaron14@gmail.com>
2025-03-19 20:06:58 -0500

    New meet 3/30

--- components/NextMeet.jsx
+++ components/NextMeet.jsx
@@ -7,7 +7,7 @@ export default function NextMeet() {
     <div className="flex flex-col items-center justify-center">
       {eventPhoto.map((photo, index) => (
               <div key={index} className="relative w-full h-full justify-center items-center align-center flex">
-                <Link href="https://www.google.com/maps/place/7-Eleven/@29.4421838,-98.664354,20.57z/data=!4m6!3m5!1s0x865c4337c646ad0d:0x505cec1e491989aa!8m2!3d29.4420603!4d-98.6642949!16s%2Fg%2F11q25sgm_s?entry=ttu&g_ep=EgoyMDI1MDIyNi4xIKXMDSoASAFQAw%3D%3Ds" target="_blank">
+                <Link href="https://www.google.com/maps/place/Lennys+Grill+%26+Subs/@29.4411918,-98.9359619,11z/data=!4m10!1m2!2m1!1slennys!3m6!1s0x865c69d6301f6c9b:0x851550ff542bcbde!8m2!3d29.5131667!4d-98.6963566!15sCgZsZW5ueXMiA4gBAVoIIgZsZW5ueXOSAQ1zYW5kd2ljaF9zaG9w4AEA!16s%2Fg%2F11kqgn8_ws?entry=ttu&g_ep=EgoyMDI1MDMxNy4wIKXMDSoASAFQAw%3D%3D" target="_blank">
                   <Image
                     src={photo.src}
                     alt={photo.alt}
